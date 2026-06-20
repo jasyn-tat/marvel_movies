5c642f1eec857bcd7813e1b5f39f52ba422e9588
jasyn-tat <jasyntatla@gmail.com>
2026-06-20 12:19:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -15,26 +15,11 @@ document.addEventListener("DOMContentLoaded", () => {
         updateRowOpacity(checkbox);
     });
 
-    // 2. SAVE PROGRESS, UPDATE OPACITY, & TRIGGER CONFETTI ON CLICK
+    // 2. SAVE PROGRESS AND UPDATE OPACITY ON CLICK
     checkboxes.forEach((checkbox) => {
-        // FIXED: Added 'e' inside the parentheses here so the browser can track the mouse click event
-        checkbox.addEventListener("change", (e) => {
+        checkbox.addEventListener("change", () => {
             localStorage.setItem(checkbox.id, checkbox.checked);
             updateRowOpacity(checkbox);
-
-            // Only shoot confetti when checking the box
-            if (checkbox.checked) {
-                // FIXED: Changed event to e to perfectly match the event listener argument above
-                const xPosition = e.clientX / window.innerWidth;
-                const yPosition = e.clientY / window.innerHeight;
-
-                confetti({
-                    particleCount: 30,
-                    spread: 60,
-                    origin: { x: xPosition, y: yPosition },
-                    colors: ['#e50914', '#ffffff', '#a0a5b5'] // Marvel Colors
-                });
-            }
         });
     });
 });
